811b6633301a55248e0e285a53cf2536eb4bc52a
lonewol7f <64887137+lonewol7f@users.noreply.github.com>
2020-06-13 13:07:07 +0530

    Create animationx.py

---
+++ userbot/plugins/animationx.py
@@ -0,0 +1,50 @@
+"""Available Commands:
+.tlol
+.eol
+.candy
+.nothappy"""
+
+from telethon import events
+import asyncio
+from collections import deque
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern=r"candy"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🍦🍧🍩🍪🎂🍰🧁🍫🍬🍭"))
+	for _ in range(999):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
+@borg.on(admin_cmd(pattern=r"nothappy"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("😁☹️😁☹️😁☹️😁"))
+	for _ in range(999):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
+@borg.on(admin_cmd(pattern=r"tlol"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🤔🧐🤨🤔🧐🤨"))
+	for _ in range(999):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
+@borg.on(admin_cmd(pattern=r"lol"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("😂🤣😂🤣😂🤣"))
+	for _ in range(999):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
